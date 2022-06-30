888fbedab8ac3838f41b3b0eb9228e5120cbcdfc
yerraez <erraez.yessenia@gmail.com>
2022-06-30 14:47:46 -0500

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
-
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 6000
-
 const app = express()
+const colors = require('colors')
+
+//Connect to database
+connectDB()
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
