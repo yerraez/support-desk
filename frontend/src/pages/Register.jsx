e5d47aba8085e000f429b17f0056d4fc7b2b9b37
yerraez <erraez.yessenia@gmail.com>
2022-07-07 09:38:15 -0500

    register user ok

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
 import {FaUser} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {register, reset} from '../features/auth/authSlice'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,6 +14,23 @@ function Register() {
     })
     
     const {name, email, password, password2} = formData
+
+    const dispatch = useDispatch()
+    const navigate  = useNavigate()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if(isError){
+            toast.error(message)
+        }
+
+        //Redirect when logged in
+        if(isSuccess || user) {
+            navigate('/')
+        }
+        dispatch(reset())
+    }, [isError, isSuccess, user, message, navigate, dispatch ])
 
     const onChange = (e) => {
         setFormData((prevState) =>({
@@ -24,6 +44,13 @@ function Register() {
 
         if(password !== password2){
             toast.error('Passwords do not match')
+        } else {
+            const userData = {
+                name, 
+                email,
+                password
+            }
+            dispatch(register(userData))
         }
     }
 
@@ -31,7 +58,7 @@ function Register() {
       <>
       <section className='heading'>
           <h1>
-              <FaUser /> Register
+              <FaUser /> Register 
           </h1>
           <p>Please create an account</p>
       </section>
