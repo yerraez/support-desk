010171e964dd1eab8cfa96e76153992b871ab290
yerraez <erraez.yessenia@gmail.com>
2022-08-29 18:05:39 -0500

    config heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
@@ -14,11 +15,22 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'hello'})
+    res.status(200).json({message: 'Welcome to the Support Desk API'})
 })
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+//Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    //Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+}else{
+    app.get('/', (req, res) => {
+        res.status(200).json({message: 'Welcome to the Support Desk API'}) })
+}
 
 app.use(errorHandler)
 
